ed3fe3b0015b8bc3cd7355b28d9deebb4642ac8c
YOAV-PC\Yoav <yoavalro@gmail.com>
2019-07-13 19:51:25 +0300

    start working on table

--- src/app/pageComponents/table/table.component.ts
+++ src/app/pageComponents/table/table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-table',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  @Input() rowsData: any[];
+  @Input() columns: string[];
+  dataSource: MatTableDataSource<any>;
+
   constructor() { }
 
   ngOnInit() {
+    this.createTable();
+  }
+
+  createTable() {
+    this.dataSource = new MatTableDataSource(this.rowsData);
   }
 
 }
